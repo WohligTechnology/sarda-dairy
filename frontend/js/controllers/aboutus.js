2bd17b1ba34191804d1de083fff1e84057ae24d0
snehalthakare <snehal.wohlig@gmail.com>
2017-04-26 10:26:32 +0530

    aboutus

--- frontend/js/controllers/aboutus.js
+++ frontend/js/controllers/aboutus.js
@@ -2,13 +2,37 @@
      $scope.template = TemplateService.getHTML("content/aboutus.html");
      TemplateService.title = "AboutUs"; //This is the Title of the Website
      $scope.navigation = NavigationService.getNavigation();
-     $scope.mySlides = [
+     $scope.myProduct = [{
+             img: 'img/aboutus/1.png',
+             text: 'Milk'
+
+         },
+         {
+             img: 'img/aboutus/2.png',
+             text: 'Dahi'
+
+         }, {
+             img: 'img/aboutus/3.png',
+             text: 'ULT Milk'
+
+         }, {
+             img: 'img/aboutus/4.png',
+             text: 'Lassi'
+
+         }, {
+             img: 'img/aboutus/4.png',
+             text: 'Milk'
+
+         }, {
+             img: 'img/aboutus/3.png',
+             text: 'Milk'
+
+         }
+     ];
+     $scope.newEvent = [
          '../img/aboutus/1.png',
          '../img/aboutus/2.png',
-         '../img/aboutus/3.png',
-         '../img/aboutus/4.png',
-         '../img/aboutus/5.png',
-         '../img/aboutus/6.png'
+         '../img/aboutus/3.png'
 
      ];
      $scope.formSubmitted = false;
